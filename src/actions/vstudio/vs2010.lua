eebd476810da264f77b58799c4371d694205c434
Manu Evans <turkeyman@gmail.com>
2013-10-28 01:45:46 +1000

    Minor changes to improve patch-ability.

--- src/actions/vstudio/vs2010.lua
+++ src/actions/vstudio/vs2010.lua
@@ -38,7 +38,7 @@
 		if premake.project.isdotnet(prj) then
 			premake.generate(prj, ".csproj", vstudio.cs2005.generate)
 			premake.generate(prj, ".csproj.user", vstudio.cs2005.generate_user)
-		else
+		elseif premake.project.iscpp(prj) then
 			premake.generate(prj, ".vcxproj", vstudio.vc2010.generate)
 			premake.generate(prj, ".vcxproj.user", vstudio.vc2010.generateUser)
 			premake.generate(prj, ".vcxproj.filters", vstudio.vc2010.generateFilters)
